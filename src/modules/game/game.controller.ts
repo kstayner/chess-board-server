09847349b41a652e4b8007f8874da022fbb6f45f
Klebson Leite <klebsonleite@MacBookPro.Home>
2025-01-09 15:28:19 +0000

    fix-017

--- src/modules/game/game.controller.ts
+++ src/modules/game/game.controller.ts
@@ -8,6 +8,8 @@ export class GameController {
   constructor(private readonly gameService: GameService) {}
 
     // comment
+    // comment 2
+
   @Post('/api/game/move')
   @UseGuards(AuthGuard)
   async validateAndExecuteMove(@Body() validateMoveDto: ValidateMoveDto) {
